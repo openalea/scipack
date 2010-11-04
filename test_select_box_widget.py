f8ce4432aab428d61e6c61c2d075f59a0aff9843
Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
2010-11-04 15:11:28 +0000

    Merging from tag tags/openalea_0_9_0 (revision 3047 including changes from CP that were made to the branch after the tag was created)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/openalea/trunk@3049 04ea39c6-a500-0410-8508-e77fe2a861cf

--- test_select_box_widget.py
+++ test_select_box_widget.py
@@ -1,17 +1,20 @@
 from PyQt4.QtGui import QApplication,QPixmap
-from select_box_widget import SelectBoxWidget
+from openalea.image_wralea.gui.select_box_widget import SelectBoxWidget
 
 class DummyNode (object) :
 	def set_input (self, *args) :
 		pass
-	
+
+	def get_input(self, *args):
+		pass
+
 	def register_listener (self, *args) :
 		pass
 
 qapp = QApplication([])
 
 w = SelectBoxWidget(DummyNode() )
-w.setPixmap(QPixmap("/home/chopard/Bureau/toto.png") )
+w.setPixmap(QPixmap("4_ocean_currents.png") )
 
 w.show()
 
